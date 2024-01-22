7e1ab2708cd4b12cad859acda04f3d7e94696eec
namazbayeva <155933085+namazbayeva@users.noreply.github.com>
2024-01-22 23:17:06 +0600

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,40 +10,34 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('MYAPP'), backgroundColor: Colors.blue,
+          title: Text('MYAPP'),
+          backgroundColor: Colors.blue,
+          centerTitle: true, 
         ),
-        backgroundColor: Colors.white, 
-        body: SingleChildScrollView( child: Stack(
-          children: [
-            Center(
-              child: Container(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
                 width: 500,
                 height: 800,
                 decoration: BoxDecoration(
                   color: Colors.blue,
-                 
                 ),
               ),
-            ),
-            Center(
-              child: Column(
+              Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   ElevatedButton(
                     onPressed: () {},
                     style: ElevatedButton.styleFrom(
-                        shadowColor: Colors.red,
-                        primary: Colors.red,
-                        fixedSize: Size(120, 120),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20))),
-                    child: Text('Button'),
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.blue,
+                      shadowColor: Colors.red,
+                      primary: Colors.red,
+                      fixedSize: Size(120, 120),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                     ),
                     child: Text('Button'),
                   ),
@@ -51,7 +45,15 @@ class MyApp extends StatelessWidget {
                   ElevatedButton(
                     onPressed: () {},
                     style: ElevatedButton.styleFrom(
-                      primary: Colors.blue,
+                      primary: Colors.green,
+                    ),
+                    child: Text('Button'),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
                     ),
                     child: Text('Button'),
                   ),
@@ -60,15 +62,77 @@ class MyApp extends StatelessWidget {
                     icon: Icon(Icons.add_to_drive_rounded),
                     label: Text("Data"),
                   ),
-
-                  
+                  SizedBox(height: 20),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.orange,
+                            ),
+                            child: Text('Button'),
+                          ),
+                          SizedBox(width: 20),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('Hello World'),
+                              Text('Hello World'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    color: Colors.pink,
+                    child: Text(
+                      'Nazerke',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        shadows: [
+                          Shadow(
+                            color: Colors.grey,
+                            offset: Offset(2.0, 2.0),
+                            blurRadius: 3.0,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                      Icon(Icons.volume_up, size: 40, color: Colors.black),
+                      SizedBox(height: 5),
+                    ],
+                  ),
+                      SizedBox(width: 10),
+                      Column(
+                        children: [
+                          SizedBox(height: 5),
+                          Text('Звук включен', style: TextStyle(fontSize: 20)),
+                        ],
+                      ),
+                    ],
+                  ),
                 ],
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
       ),
     );
   }
 }
+
+
